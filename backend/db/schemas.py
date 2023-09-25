c5fc935a0949668dbd547e7dc7d2cd9f9a198819
CodingForEntrepreneurs <hello@teamcfe.com>
2023-09-25 16:44:20 -0600

    32 - Detail View and Schema with FastAPI and Pydantic

--- backend/db/schemas.py
+++ backend/db/schemas.py
@@ -6,6 +6,21 @@ class FlightPriceSchema(BaseModel): # pydantic
     flightDate: date
     startingAirport: str
     destinationAirport: str
+    # isBasicEconomy: bool
+    # isRefundable: bool
+    # isNonStop: bool
+    segmentsAirlineName: str
+    totalFare: int
+
+    class Config:
+        from_attributes = True
+
+
+class FlightPriceDetailSchema(BaseModel): # pydantic
+    id: int 
+    flightDate: date
+    startingAirport: str
+    destinationAirport: str
     isBasicEconomy: bool
     isRefundable: bool
     isNonStop: bool
--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,10 @@
 from typing import List
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
 from backend.env import config
 from backend.db.connect import get_db_session, SessionLocal
-from backend.db.schemas import FlightPriceSchema
+from backend.db.schemas import FlightPriceSchema, FlightPriceDetailSchema
 from backend.db import utils as db_utils
 
 DEBUG = config("DEBUG", cast=bool, default=False)
@@ -36,5 +36,13 @@ def write_hello_world():
 
 
 @app.get("/flights/", response_model=List[FlightPriceSchema])
-def read_flight_prices(db_session: SessionLocal = Depends(get_db_session)):
-    return db_utils.get_flight_prices(db_session)
+def read_flight_prices(offset:int=0, limit:int=100, db_session: SessionLocal = Depends(get_db_session)):
+    return db_utils.get_flight_prices(db_session, offset=offset, limit=limit)
+
+
+@app.get("/flights/{flight_price}", response_model=FlightPriceDetailSchema)
+def read_flight_prices(flight_price:int, db_session: SessionLocal = Depends(get_db_session)):
+    db_flight_value = db_utils.get_flight_price(db_session, flight_price)
+    if db_flight_value is None:
+        raise HTTPException(status_code=404, detail='Flight Price not Found')
+    return db_flight_value
