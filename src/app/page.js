318d4c179ca6f4b79bbfa3c0b676ea5de33f1fe7
CodingForEntrepreneurs <hello@teamcfe.com>
2023-09-25 16:54:05 -0600

    33 - Listing API Data with Nextjs

--- src/app/page.js
+++ src/app/page.js
@@ -5,17 +5,35 @@ import useSWR from 'swr'
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 export default function Home() {
-  const url = "http://localhost:8080/hello-world"
+  const url = "http://localhost:8080/flights"
   const { data, error, isLoading } = useSWR(url, fetcher)
   const mainClassCSS ="flex min-h-screen flex-col items-center justify-between p-24"
   if (error) return <div className={mainClassCSS}>failed to load</div>
   if (isLoading) return <div className={mainClassCSS}>loading...</div>
   console.log(data)
   const myVar = "word"
+
+  const renderListData = (row, idx) => {
+    const flightRowLink = `/flights/${row.id}`
+    return <div key={`flight-data-${idx}`}>
+        <p>
+          <a href={flightRowLink}>{row.flightDate}</a>
+        </p>
+        <p>
+          {row.startingAirport}
+        </p>
+        <p> 
+          {row.startingAirport}
+        </p>
+        <p>
+          {row.totalFare}
+        </p>
+    </div>
+  }
   return ( // jsx
     <main className={mainClassCSS}>
       <h1>hello {myVar}</h1>
-      <div>{JSON.stringify(data)}</div>
+      {data && data.map(renderListData)}
     </main>
   )
 }
