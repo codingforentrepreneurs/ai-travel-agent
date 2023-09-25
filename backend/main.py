485e2a16895a23dbbc8065171f4466b159ded282
CodingForEntrepreneurs <hello@teamcfe.com>
2023-09-25 12:55:51 -0600

    29 - FastAPI CORS for Nextjs

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from backend.env import config
 
 DEBUG = config("DEBUG", cast=bool, default=False)
+FRONTEND_ORIGINS = config("FRONTEND_ORIGINS", cast=lambda x: [s.strip() for s in x.split(",")], default="")
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=FRONTEND_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/hello-world")
 def read_hello_world():
